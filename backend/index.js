f7d20617a7a1022a17ac146c9a44ed5f8acc00e3
piyush-xo <kpiyush381@gmail.com>
2025-07-06 17:15:42 +0530

    database connection setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mysql = require("mysql2/promise");
+const {dbConfig} = require("./config");
 
 const app = express();
+
+async function testDBConnection() {
+  try {
+    console.log(dbConfig);
+    const conn = mysql.createPool(dbConfig);
+    console.log("connected");
+    await conn.end();
+  } catch (err) {
+    console.error("failed", err.message);
+  }
+}
+testDBConnection();
 
 app.listen(5000, () => console.log("Server is running at 5000"));
